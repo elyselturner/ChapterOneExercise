d0bbfbc1072d9211d0b750e8fd6a076ed700c748
elyselturner <elyse.turner@detroitlabs.com>
2014-10-06 09:21:51 -0400

    adding the true false class

---
+++ app/src/main/java/com/example/elyseturner/chapteroneexercise/TrueFalse.java
@@ -0,0 +1,15 @@
+package com.example.elyseturner.chapteroneexercise;
+
+/**
+ * Created by elyseturner on 10/6/14.
+ */
+public class TrueFalse {
+    private int mQuestion;
+
+    private boolean mTrueQuestion;
+
+    public TrueFalse(int question, boolean trueQuestion){
+        mQuestion = question;
+        mTrueQuestion = trueQuestion;
+    }
+}
